236df7af7d68755f58cdd986acb684b0f80b0605
Wesley-S-Alves <75279678+Wesley-S-Alves@users.noreply.github.com>
2022-02-15 11:20:15 -0300

    Update questao3.java

--- src/main/java/questao3.java
+++ src/main/java/questao3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class questao3 {
     public static void main(String[] args) {
         System.out.println("-----------Contador de Anagramas--------------");
-        System.out.print("insira a palavra para análise: ");
+        System.out.println("insira a palavra para análise: ");
         Scanner sc = new Scanner(System.in);
         String anagrama = sc.next();
         procuraAnagrama(anagrama);
